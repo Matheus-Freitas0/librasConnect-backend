7bbcf522644565e6883c432f07be936a597fa91c
Matheus-Freitas0 <moncaomatheus@gmail.com>
2026-05-16 11:19:30 -0300

    Add custom query to SignSampleRepository for retrieving all SignSample entities

--- src/main/java/com/librasConnect/system/repositories/SignSampleRepository.java
+++ src/main/java/com/librasConnect/system/repositories/SignSampleRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.librasConnect.system.models.SignSample;
 
 public interface SignSampleRepository extends JpaRepository<SignSample, String> {
 
     @EntityGraph(attributePaths = "sign")
+    @Query("SELECT s FROM SignSample s")
     List<SignSample> findAllWithSign();
 
     List<SignSample> findBySign_IdOrderByCreatedAtAsc(String signId);
